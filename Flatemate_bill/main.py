1d173bd645a3c0540c79a416a52d40fe60b4f011
Jaya Singh <137374743+jayasinghthakur@users.noreply.github.com>
2024-06-21 10:26:41 +0530

    Update main.py

--- Flatemate_bill/main.py
+++ Flatemate_bill/main.py
@@ -1,3 +1,6 @@
+import webbrowser
+
+from fpdf import FPDF
 class Bill:
     """
     Object that contains data about a bill, such as total amount and period of the bill
@@ -33,11 +36,30 @@ class PdfReport:
         self.filename = filename
 
     def generate(self, flatmate1, flatmate2, bill):
-        pass
+        pdf = FPDF(orientation="P" ,unit="pt" ,format="A4")
+        pdf.add_page()
 
+        pdf.image("house.png",w=30,h=30)
+
+        pdf.set_font("Times","B",24)
+        pdf.cell(w=0,h=80,txt="Flatmate bill" ,align="C",ln=1)
+
+        pdf.cell(w=100,h=40,txt="Period:")
+        pdf.cell(w=150,h=40,txt=bill.period,ln=1)
+
+        pdf.cell(w=100,h=40,txt=flatmate1.name)
+        pdf.cell(w=150,h=40,txt=str(round(flatmate1.pays(bill,flatmate2),2)),ln=1)
+        pdf.cell(w=100, h=40,txt=flatmate2.name)
+        pdf.cell(w=150,h=40,txt=str(round(flatmate2.pays(bill,flatmate1),2)),ln=1)
+
+        pdf.output(self.filename)
+        webbrowser.open(self.filename)
 
 the_bill = Bill(120, "March 2024")
 jai = Flatmate("Jai", 20)
 jaya = Flatmate("Jaya", 25)
 print("jai pays", jai.pays(the_bill, jaya))
 print("jaya pays", jaya.pays(the_bill, jai))
+
+pdf_report = PdfReport("Report.pdf")
+pdf_report.generate(jai,jaya,the_bill)
